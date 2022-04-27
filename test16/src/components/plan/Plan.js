5e78f0dd9efeb03d28652a4453db0fed759e3a19
kyulgon <83215027+nlee013@users.noreply.github.com>
2022-04-27 17:59:30 +0900

    add

--- test16/src/components/plan/Plan.js
+++ test16/src/components/plan/Plan.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import GlobalStyle from '../styled/Global'
 import PlanAdd from './PlanAdd';
 import PlanList from './PlanList';
@@ -6,28 +6,76 @@ import dataList from './data';
 
 const Plan = () => {
 
-    const no = useRef(dataList.length + 1)
+    //const no = useRef(dataList.length + 1)
 
-    const [plans, setPlans] = useState(dataList)
+    //const [plans, setPlans] = useState(dataList)
+
+    const [plans,setPlans] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/plans')
+        .then(res=>res.json())
+        .then(res=>setPlans(res))
+    }, [])
 
     const onDel = (id) => {
+        fetch(`http://localhost:4000/plans/${id}`, {
+            method:'delete'
+        })
+        //화면삭제
         setPlans(plans.filter(plan=>plan.id !== id))
     }
 
-    const onToggle = (id) => {
-        setPlans(plans.map(plan=>plan.id === id ? {
-            ...plan,
-            done:!plan.done
-        }: plan))
+    const fetchPlan = async(id) => {
+        const res = await fetch(`http://localhost:4000/plans/${id}`)
+        const data = await res.json
+        return data
     }
 
-    const onAdd = (form) => {
+    const onToggle = async(id) => {
+        //json data를 꺼내와서 수정할 예정
+        const togglePlan = await fetchPlan(id)
+        const updatePlan = {
+                                ...togglePlan,
+                                done:!togglePlan.done
+                            }
+        //수정한 데이터를 다시 넣기
+        const res = await fetch(`http://localhost:4000/plans/${id}`,{
+            method:'put',
+            headers:{
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify(updatePlan)
+        })
+        // setPlans(plans.map(plan=>plan.id === id ? {
+        //     ...plan,
+        //     done:!plan.done
+        // }: plan))
 
-        form.id = no.current++
-        setPlans([
+        //화면 출력
+        getToggle()
+    }
+
+    const getToggle = () => {
+        const config = {method:'get'}
+        fetch('http://localhost:4000/plans/', config)
+        .then(res=>res.json())
+        .then(res=>res.setPlans(res))
+    }
+    const onAdd = (form) => {
+        //form.id = no.current++
+        fetch('http://localhost:4000/plans',{
+            method:'post',
+            headers:{
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify(form)
+        })
+        .then(res=>res.json())
+        .then(res=>setPlans([
             ...plans,
-            form
-        ])
+            res
+        ]))
     }
     return (
         <div>
@@ -39,3 +87,7 @@ const Plan = () => {
 };
 
 export default Plan;
+
+/*
+
+*/
